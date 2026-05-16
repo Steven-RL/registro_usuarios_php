bd5d7785fb20deb183b4340bc7138766ed11d66a
Steven-RL <01stebben@gmail.com>
2026-05-16 13:26:40 -0500

    Creo y agrego un footer para evitar escribir mas codigo en las paginas.php

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer class="bg-dark bg-opacity-90 text-white-50 py-3 mt-auto">
+    <div class="container">
+        <!-- Fila 1: Copyright centrado -->
+        <div class="row">
+            <div class="col-12 text-center">
+                <p class="mb-0 fs-7">&copy; <span class="text-white">Sesiones con PHP y phpMyAdmin</span> - Todos los derechos reservados.</p>
+            </div>
+        </div>
+        <!-- Fila 2: Dos columnas -->
+        <div class="row justify-content-center mt-3">
+            <!-- Columna izquierda con margen lateral -->
+            <div class="col-12 col-md-auto mx-5">
+                <div class="text-center text-md-start">
+                    <div class="mb-2">
+                        <span class="small fw-semibold">Síguenos en</span>
+                    </div>
+                    <div class="d-flex flex-wrap justify-content-center justify-content-md-start gap-3">
+                        <a href="#" class="btn btn-outline-secondary border-0 p-0 text-white-50 small text-decoration-underline">
+                            <i class="bi bi-facebook me-1"></i> Facebook
+                        </a>
+                        <a href="#" class="btn btn-outline-secondary border-0 p-0 text-white-50 small text-decoration-underline">
+                            <i class="bi bi-github me-1"></i> GitHub
+                        </a>
+                        <a href="#" class="btn btn-outline-secondary border-0 p-0 text-white-50 small text-decoration-underline">
+                            <i class="bi bi-twitter-x me-1"></i> Twitter
+                        </a>
+                    </div>
+                    <div class="mt-2">
+                        <a href="mailto:osrivadeneira@utpl.edu.ec" class="text-decoration-none d-inline-flex align-items-center"><i class="bi bi-envelope-at me-2"></i> osrivadeneira@utpl.edu.ec
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Columna derecha  -->
+            <div class="col-12 col-md-auto mx-5">
+                <div class="text-center text-md-start">
+                    <div class="mb-2">
+                        <span class="small fw-semibold">Recursos</span>
+                    </div>
+                    <ul class="list-unstyled small mb-0">
+                        <li class="mb-1">
+                            <a href="https://www.w3schools.com/php/" target="_blank" class="text-white-50 text-decoration-none">
+                                <i class="bi bi-box-arrow-up-right me-1"></i> W3Schools PHP
+                            </a>
+                        </li>
+                        <li class="mb-1">
+                            <a href="https://www.php.net/manual/es/book.session.php" target="_blank" class="text-white-50 text-decoration-none">
+                                <i class="bi bi-box-arrow-up-right me-1"></i> Manual de Sesiones PHP
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://getbootstrap.com/" target="_blank" class="text-white-50 text-decoration-none">
+                                <i class="bi bi-box-arrow-up-right me-1"></i> Bootstrap
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
